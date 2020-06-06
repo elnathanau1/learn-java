cb4c664912c37da55fbfaa0038864ca7ec59dc6a
elnathanau1 <elnathanau1@gmail.com>
2020-06-06 16:38:53 -0500

    Spring initializr

---
+++ src/main/java/eau/projects/learnjava/LearnJavaApplication.java
@@ -0,0 +1,13 @@
+package eau.projects.learnjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJavaApplication.class, args);
+	}
+
+}
